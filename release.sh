4cfbd3d29f3a0c1fc04eb1d266330d8eb6a3c5be
Willy Blandin <wblandin@gmail.com>
2014-09-30 15:44:15 +0200

    improve release script

--- release.sh
+++ release.sh
@@ -21,6 +21,7 @@ cd $DIR
 echo "/* microphone.js $VER */" | cat - microphone.js > microphone.min.js
 echo "/* microphone.css $VER */" | cat - microphone.css > microphone.min.css
 rm microphone.{js,css}
+rm index.html
 cd -
 
 # zip it
@@ -35,10 +36,17 @@ read GH_USER
 echo -n "github password: "
 read -s GH_PW
 echo
-curl -XPOST https://api.github.com/repos/wit-ai/microphone/releases -d '{
+# create release
+RELEASE_ID=$(curl -XPOST https://api.github.com/repos/wit-ai/microphone/releases -d '{
   "tag_name": "'$VER'",
-  "name": "'$TAR'"
-}' -u "$GH_USER:$GH_PW"
+  "name": "'$VER'"
+}' -s -u "$GH_USER:$GH_PW" | jq --raw-output '.id')
+
+# upload archive
+TAR_PATH=$(dirname $DIR)/$TAR
+echo "uploading $TAR_PATH to https://api.github.com/repos/wit-ai/microphone/releases/$RELEASE_ID/assets?name=$TAR"
+curl -XPOST "https://uploads.github.com/repos/wit-ai/microphone/releases/$RELEASE_ID/assets?name=$TAR" \
+  -H 'Content-Type: application/octet-stream' -s --data-binary "@$TAR_PATH" -u "$GH_USER:$GH_PW"
 set -x
 
 # clean
